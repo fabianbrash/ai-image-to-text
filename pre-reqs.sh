7e662f7ce6f15ad85072fe89d4eb9c9ac18a0ce1
Fabian Brash <fabianbrash@gmail.com>
2025-02-20 10:50:13 -0500

    Add files via upload

---
+++ pre-reqs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#pip install -r requirements.txt
+
+# I needed this to be installed on wsl Ubuntu 22.04 it might not be needed for a regular ubuntu 22.04 machnine
+# Also I'm using an NVIDIA GPU so this might have been required for 1 of the libraries I installed
+
+#sudo apt-get update && sudo apt-get install -y libgl1 libglib2.0-0
