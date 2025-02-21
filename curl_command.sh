7a32f8dd13e71fd74860fb3b49fa28a2165b6f3e
Fabian Brash <fabianbrash@gmail.com>
2025-02-21 12:00:01 -0500

    Update curl_command.sh

--- curl_command.sh
+++ curl_command.sh
@@ -2,3 +2,6 @@
 
 
 curl -X POST -F "image=@/home/frjb-nix/workspace/python/ai-ml/image-to-text/dataset/train/images/yinger.png" http://localhost:8000/predict/
+
+
+curl -LO https://fb-com-static-assets.s3.us-east-1.amazonaws.com/dataset.tar
